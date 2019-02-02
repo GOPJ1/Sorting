12295a4edf5f3b931d01cdae20c509a1b33dc5cc
John Knecht V <jk0125@live.com>
2019-02-02 17:56:08 -0500

    Added documentation

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,6 +2,11 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
+    /**
+     * Bubble sort algorithm. Loops over the array and compares adjacent indexes. If the lower indexes value is greater than the higher
+     * indexes value, switch the position of the elements.
+     * @param array - Array being sorted.
+     */
     private static void bubbleSort(int array[]){
 
         //Keeps track of the arrays length
@@ -21,12 +26,25 @@ public class BubbleSort {
         }
     }
 
+    /**
+     * Given two indexes and array, switch the positions of the elements.
+     * @param index1 - First index to be switched
+     * @param index2 - Second index to be switched
+     * @param array - Array having it's items switched
+     */
     private static void swapNumbers(int index1, int index2, int array[]){
+        //Save the first value for later use
         int temp = array[index1];
+
+        //Switch the elements at the given indexes
         array[index1] = array[index2];
         array[index2] = temp;
     }
 
+    /**
+     * Prints the supplied array before and after sorting. Calculates how long the sorting takes.
+     * @param array - Array being sorted
+     */
     private static void printResults(int array[]){
         System.out.println("Pre-sorted: " + Arrays.toString(array));
         long startTime = System.nanoTime();
@@ -36,6 +54,8 @@ public class BubbleSort {
         long executionTime = endTime-startTime;
         System.out.println("Sort time: " + executionTime/1000 + " seconds.\n");
     }
+
+
     public static void main(String[] args) {
         int[] array1 = {1, 4, 6, 10, 14, 55, 7, 2, 43, 5, 32};
         printResults(array1);
